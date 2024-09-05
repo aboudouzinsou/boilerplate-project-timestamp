00e7877ae003669f59b5d62cec41a34ee94f2779
aboudouzinsou <faboudouzinsou@gmail.com>
2024-09-05 15:57:31 +0200

    microservice setup
    api route for timestamp microservice created

--- index.js
+++ index.js
@@ -13,15 +13,38 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
+app.use(express.json());
+
+function formatDateToUTC(date) {
+  return new Date(date).toUTCString();
+}
+
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// API endpoint to get the current time in UTC
+app.get('/api/:date?', (req, res) => {
+  const date = req.params.date;
+  const utcDate = formatDateToUTC(date);
+
+  if(!date) {
+    date = new Date();
+  } else if (!isNaN(date)) {
+    date = new Date(parseInt(date));
+  } else {
+    date = new Date(date);
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+  
+  res.json({ unix: utcDate.getTime(), utc: utcDate });
 });
 
 
